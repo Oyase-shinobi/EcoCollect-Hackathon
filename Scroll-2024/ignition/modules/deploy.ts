c88227185974de973b4185029191c528b52c01aa
Oyase-shinobi <donjosy305@gmail.com>
2024-09-30 06:39:58 +0100

    update

--- Scroll-2024/ignition/modules/deploy.ts
+++ Scroll-2024/ignition/modules/deploy.ts
@@ -3,7 +3,11 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 const LockModule = buildModule("LockModule", (m) => {
 
   const token = m.contract("OGToken");
-  const tBazaar = m.contract("TrustBazaar");
+
+  const admin = "0x186a761645f2A264ad0A655Fb632Ca99150803A9";
+  const reciever = "0x40feacdeee6f017fA2Bc1a8FB38b393Cf9022d71"
+
+  const tBazaar = m.contract("TrustBazaar", [admin, reciever, token]);
 
   return { token, tBazaar };
 });
