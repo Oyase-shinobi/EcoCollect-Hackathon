ca666dd2392f6e37d2a283c6ddcba268ca054046
Oyase-shinobi <donjosy305@gmail.com>
2024-09-30 07:52:26 +0100

    updated

--- Scroll-2024/hardhat.config.ts
+++ Scroll-2024/hardhat.config.ts
@@ -17,11 +17,26 @@ const config: HardhatUserConfig = {
     }
   },
   networks: {
-    scrollTestnet: {
-      url: process.env.SCROLL_ALCHEMY_URL || "",
+    scrollSepolia: {
+      url: process.env.SCROLL_ALCHEMY_URL || 'https://sepolia-rpc.scroll.io',
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
+  },
+  etherscan: {
+    apiKey: {
+      scrollSepolia:  "SEIAVN8JJ3VWFCVBFV2DQDG1WDAM3CPDEW",
+    },
+    customChains: [
+      {
+        network: 'scrollSepolia',
+        chainId: 534351,
+        urls: {
+          apiURL: 'https://api-sepolia.scrollscan.com/api',
+          browserURL: 'https://sepolia.scrollscan.com/',
+        },
+      },
+    ],
   },
 };
 
